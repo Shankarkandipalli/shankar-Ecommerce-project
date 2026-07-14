0137e554575ac60b77a6f71b955fba1b8a00e256
Shankar <shankerteja632@example.com>
2026-07-14 17:58:58 +0530

    git push Origin

---
+++ src/main/java/com/shankar/Ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.shankar.Ecommerce.exception;
+
+import com.shankar.Ecommerce.dtos.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handlerUnknownException(Exception ex) {
+        ApiResponse<?> errorResponse = ApiResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(ex.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handlerNotFoundException(NotFoundException ex) {
+        ApiResponse<?> errorResponse = ApiResponse.builder().status(HttpStatus.NOT_FOUND.value()).message(ex.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler(InvlidCredentialsException.class)
+    public ResponseEntity<ApiResponse<?>> handlerInvlidCredentialsException(InvlidCredentialsException ex) {
+        ApiResponse<?> errorResponse = ApiResponse.builder().status(HttpStatus.BAD_REQUEST.value()).message(ex.getMessage()).build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+    }
+
+
+}
---
+++ src/main/java/com/shankar/Ecommerce/exception/InvlidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.shankar.Ecommerce.exception;
+
+public class InvlidCredentialsException extends RuntimeException{
+    public InvlidCredentialsException(String message) {
+        super(message);
+    }
+}
